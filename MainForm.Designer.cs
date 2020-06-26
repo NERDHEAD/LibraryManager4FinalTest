38ca3809892dda5439c5a1f4529169cd4f023c14
Nerdhead <wntjd337@gmail.com>
2020-06-26 15:46:31 +0900

    Update : Gui

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -30,25 +30,25 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.managePage = new System.Windows.Forms.TabPage();
-            this.button10 = new System.Windows.Forms.Button();
-            this.button9 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnBookApply = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEditMode = new System.Windows.Forms.Button();
+            this.btnInfo = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.cbSearchList = new System.Windows.Forms.ComboBox();
             this.revenuePage = new System.Windows.Forms.TabPage();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btnBookList = new System.Windows.Forms.Button();
+            this.btnApplyList = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.button11 = new System.Windows.Forms.Button();
+            this.btnSalesList = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.managePage.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -67,17 +67,17 @@
             // 
             // managePage
             // 
-            this.managePage.Controls.Add(this.button10);
-            this.managePage.Controls.Add(this.button9);
-            this.managePage.Controls.Add(this.button8);
-            this.managePage.Controls.Add(this.button7);
-            this.managePage.Controls.Add(this.button5);
-            this.managePage.Controls.Add(this.button6);
-            this.managePage.Controls.Add(this.button4);
-            this.managePage.Controls.Add(this.button3);
-            this.managePage.Controls.Add(this.dataGridView1);
-            this.managePage.Controls.Add(this.textBox1);
-            this.managePage.Controls.Add(this.comboBox1);
+            this.managePage.Controls.Add(this.btnBookApply);
+            this.managePage.Controls.Add(this.btnUpdate);
+            this.managePage.Controls.Add(this.btnInsert);
+            this.managePage.Controls.Add(this.btnDelete);
+            this.managePage.Controls.Add(this.btnEditMode);
+            this.managePage.Controls.Add(this.btnInfo);
+            this.managePage.Controls.Add(this.btnReset);
+            this.managePage.Controls.Add(this.btnSearch);
+            this.managePage.Controls.Add(this.dataGridView);
+            this.managePage.Controls.Add(this.txtSearch);
+            this.managePage.Controls.Add(this.cbSearchList);
             this.managePage.Location = new System.Drawing.Point(4, 5);
             this.managePage.Name = "managePage";
             this.managePage.Padding = new System.Windows.Forms.Padding(3);
@@ -85,108 +85,108 @@
             this.managePage.TabIndex = 0;
             this.managePage.UseVisualStyleBackColor = true;
             // 
-            // button10
+            // btnBookApply
             // 
-            this.button10.Location = new System.Drawing.Point(738, 11);
-            this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(79, 31);
-            this.button10.TabIndex = 10;
-            this.button10.Text = "도서 신청";
-            this.button10.UseVisualStyleBackColor = true;
+            this.btnBookApply.Location = new System.Drawing.Point(738, 11);
+            this.btnBookApply.Name = "btnBookApply";
+            this.btnBookApply.Size = new System.Drawing.Size(79, 31);
+            this.btnBookApply.TabIndex = 10;
+            this.btnBookApply.Text = "도서 신청";
+            this.btnBookApply.UseVisualStyleBackColor = true;
             // 
-            // button9
+            // btnUpdate
             // 
-            this.button9.Font = new System.Drawing.Font("굴림", 8F);
-            this.button9.Location = new System.Drawing.Point(773, 299);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(44, 21);
-            this.button9.TabIndex = 9;
-            this.button9.Text = "수정";
-            this.button9.UseVisualStyleBackColor = true;
+            this.btnUpdate.Font = new System.Drawing.Font("굴림", 8F);
+            this.btnUpdate.Location = new System.Drawing.Point(773, 299);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(44, 21);
+            this.btnUpdate.TabIndex = 9;
+            this.btnUpdate.Text = "수정";
+            this.btnUpdate.UseVisualStyleBackColor = true;
             // 
-            // button8
+            // btnInsert
             // 
-            this.button8.Font = new System.Drawing.Font("굴림", 8F);
-            this.button8.Location = new System.Drawing.Point(773, 272);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(44, 21);
-            this.button8.TabIndex = 8;
-            this.button8.Text = "등록";
-            this.button8.UseVisualStyleBackColor = true;
+            this.btnInsert.Font = new System.Drawing.Font("굴림", 8F);
+            this.btnInsert.Location = new System.Drawing.Point(773, 272);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(44, 21);
+            this.btnInsert.TabIndex = 8;
+            this.btnInsert.Text = "등록";
+            this.btnInsert.UseVisualStyleBackColor = true;
             // 
-            // button7
+            // btnDelete
             // 
-            this.button7.Font = new System.Drawing.Font("굴림", 8F);
-            this.button7.Location = new System.Drawing.Point(773, 326);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(44, 21);
-            this.button7.TabIndex = 7;
-            this.button7.Text = "삭제";
-            this.button7.UseVisualStyleBackColor = true;
+            this.btnDelete.Font = new System.Drawing.Font("굴림", 8F);
+            this.btnDelete.Location = new System.Drawing.Point(773, 326);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(44, 21);
+            this.btnDelete.TabIndex = 7;
+            this.btnDelete.Text = "삭제";
+            this.btnDelete.UseVisualStyleBackColor = true;
             // 
-            // button5
+            // btnEditMode
             // 
-            this.button5.Location = new System.Drawing.Point(738, 353);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(79, 31);
-            this.button5.TabIndex = 6;
-            this.button5.Text = "편집 모드";
-            this.button5.UseVisualStyleBackColor = true;
+            this.btnEditMode.Location = new System.Drawing.Point(738, 353);
+            this.btnEditMode.Name = "btnEditMode";
+            this.btnEditMode.Size = new System.Drawing.Size(79, 31);
+            this.btnEditMode.TabIndex = 6;
+            this.btnEditMode.Text = "편집 모드";
+            this.btnEditMode.UseVisualStyleBackColor = true;
             // 
-            // button6
+            // btnInfo
             // 
-            this.button6.Font = new System.Drawing.Font("굴림", 9F);
-            this.button6.Location = new System.Drawing.Point(738, 390);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(79, 43);
-            this.button6.TabIndex = 5;
-            this.button6.Text = "정보 보기";
-            this.button6.UseVisualStyleBackColor = true;
+            this.btnInfo.Font = new System.Drawing.Font("굴림", 9F);
+            this.btnInfo.Location = new System.Drawing.Point(738, 390);
+            this.btnInfo.Name = "btnInfo";
+            this.btnInfo.Size = new System.Drawing.Size(79, 43);
+            this.btnInfo.TabIndex = 5;
+            this.btnInfo.Text = "정보 보기";
+            this.btnInfo.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // btnReset
             // 
-            this.button4.Location = new System.Drawing.Point(657, 11);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 31);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "초기화";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btnReset.Location = new System.Drawing.Point(657, 11);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 31);
+            this.btnReset.TabIndex = 4;
+            this.btnReset.Text = "초기화";
+            this.btnReset.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btnSearch
             // 
-            this.button3.Location = new System.Drawing.Point(576, 11);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 31);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "검색";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnSearch.Location = new System.Drawing.Point(576, 11);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 31);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "검색";
+            this.btnSearch.UseVisualStyleBackColor = true;
             // 
-            // dataGridView1
+            // dataGridView
             // 
-            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(16, 49);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 23;
-            this.dataGridView1.Size = new System.Drawing.Size(716, 384);
-            this.dataGridView1.TabIndex = 2;
+            this.dataGridView.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(16, 49);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowTemplate.Height = 23;
+            this.dataGridView.Size = new System.Drawing.Size(716, 384);
+            this.dataGridView.TabIndex = 2;
             // 
-            // textBox1
+            // txtSearch
             // 
-            this.textBox1.Font = new System.Drawing.Font("굴림", 15F);
-            this.textBox1.Location = new System.Drawing.Point(125, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(445, 30);
-            this.textBox1.TabIndex = 1;
+            this.txtSearch.Font = new System.Drawing.Font("굴림", 15F);
+            this.txtSearch.Location = new System.Drawing.Point(125, 12);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(445, 30);
+            this.txtSearch.TabIndex = 1;
             // 
-            // comboBox1
+            // cbSearchList
             // 
-            this.comboBox1.Font = new System.Drawing.Font("굴림", 15F);
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(16, 12);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(103, 28);
-            this.comboBox1.TabIndex = 0;
+            this.cbSearchList.Font = new System.Drawing.Font("굴림", 15F);
+            this.cbSearchList.FormattingEnabled = true;
+            this.cbSearchList.Location = new System.Drawing.Point(16, 12);
+            this.cbSearchList.Name = "cbSearchList";
+            this.cbSearchList.Size = new System.Drawing.Size(103, 28);
+            this.cbSearchList.TabIndex = 0;
             // 
             // revenuePage
             // 
@@ -197,25 +197,25 @@
             this.revenuePage.TabIndex = 1;
             this.revenuePage.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // btnBookList
             // 
-            this.button1.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.button1.Location = new System.Drawing.Point(9, 54);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(79, 43);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "도서 목록";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnBookList.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnBookList.Location = new System.Drawing.Point(9, 54);
+            this.btnBookList.Name = "btnBookList";
+            this.btnBookList.Size = new System.Drawing.Size(79, 43);
+            this.btnBookList.TabIndex = 0;
+            this.btnBookList.Text = "도서 목록";
+            this.btnBookList.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnApplyList
             // 
-            this.button2.Font = new System.Drawing.Font("굴림", 9F);
-            this.button2.Location = new System.Drawing.Point(9, 104);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(79, 43);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "신청 목록";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnApplyList.Font = new System.Drawing.Font("굴림", 9F);
+            this.btnApplyList.Location = new System.Drawing.Point(9, 104);
+            this.btnApplyList.Name = "btnApplyList";
+            this.btnApplyList.Size = new System.Drawing.Size(79, 43);
+            this.btnApplyList.TabIndex = 1;
+            this.btnApplyList.Text = "신청 목록";
+            this.btnApplyList.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
@@ -226,32 +226,32 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "메     뉴";
             // 
-            // button11
+            // btnSalesList
             // 
-            this.button11.Font = new System.Drawing.Font("굴림", 9F);
-            this.button11.Location = new System.Drawing.Point(9, 395);
-            this.button11.Name = "button11";
-            this.button11.Size = new System.Drawing.Size(79, 43);
-            this.button11.TabIndex = 3;
-            this.button11.Text = "도서 매출";
-            this.button11.UseVisualStyleBackColor = true;
+            this.btnSalesList.Font = new System.Drawing.Font("굴림", 9F);
+            this.btnSalesList.Location = new System.Drawing.Point(9, 395);
+            this.btnSalesList.Name = "btnSalesList";
+            this.btnSalesList.Size = new System.Drawing.Size(79, 43);
+            this.btnSalesList.TabIndex = 3;
+            this.btnSalesList.Text = "도서 매출";
+            this.btnSalesList.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(927, 450);
-            this.Controls.Add(this.button11);
+            this.Controls.Add(this.btnSalesList);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnApplyList);
+            this.Controls.Add(this.btnBookList);
             this.Controls.Add(this.tabControl1);
             this.Name = "MainForm";
             this.Text = "도서관리프로그램";
             this.tabControl1.ResumeLayout(false);
             this.managePage.ResumeLayout(false);
             this.managePage.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -261,21 +261,21 @@
 
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage managePage;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.ComboBox cbSearchList;
         private System.Windows.Forms.TabPage revenuePage;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnBookList;
+        private System.Windows.Forms.Button btnApplyList;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button10;
-        private System.Windows.Forms.Button button9;
-        private System.Windows.Forms.Button button8;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnBookApply;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEditMode;
+        private System.Windows.Forms.Button btnInfo;
+        private System.Windows.Forms.Button btnSalesList;
     }
 }
